6c72a4ca9e0363e385a13d7e5ad6f0371b66f343
gissestephy <gisela.s.colmeiro@gmail.com>
2023-03-19 20:00:07 -0300

    Ejercicio Integrador

---
+++ consigna7.py
@@ -0,0 +1,83 @@
+# 7. Crea una clase llamada Cuenta que tendrá los siguientes atributos: titular (que es una
+# persona) y cantidad (puede tener decimales). El titular será obligatorio y la cantidad es
+# opcional. Crear los siguientes métodos para la clase:
+# - Un constructor, donde los datos pueden estar vacíos.
+# - Los setters y getters para cada uno de los atributos. El atributo no se puede modificar
+# directamente, sólo ingresando o retirando dinero.
+# - mostrar(): Muestra los datos de la cuenta.
+# - ingresar(cantidad): se ingresa una cantidad a la cuenta, si la cantidad introducida es
+# negativa, no se hará nada.
+# - retirar(cantidad): se retira una cantidad a la cuenta. La cuenta puede estar en números rojos
+
+
+class Persona:
+    def __init__(self, nombre, apellido):
+        self.__nombre = nombre
+        self.__apellido = apellido
+        
+    @property
+    def nombre(self):
+        return self.__nombre
+    
+    @nombre.setter
+    def nombre(self,valor):
+        self.__nombre = valor
+        
+    @property
+    def apellido(self):
+        return self.__apellido
+    
+    @apellido.setter
+    def apellido(self, valor):
+        self.__apellido = valor 
+
+    @property
+    def nombre_completo(self):
+        return f"El titular de la cuenta es {self.__nombre} {self.__apellido}"
+
+#Se crea una clase llamada Cuenta con 2 atributos titular y cantidad
+class Cuenta():
+    def __init__(self,titular, cantidad):
+        self.titular= titular
+        self.__cantidad= 0
+        
+    def get_titular(self):
+        return self.titular
+    
+    def set_titular(self):
+        return self.titular
+    
+    def set_cantidad(self, cantidad):
+        self.__cantidad = cantidad
+    
+    def get_cantidad(self):
+        return self.__cantidad
+ 
+#Se muestran los datos de la cuenta   
+    def mostrar(self):
+        print("Titular: ", self.titular)
+        print("Cantidad: ", self.__cantidad)
+    
+#Se realizan dos operaciones cuando Ingresa dinero y cuando Retira dinero
+    def ingresar(self, cantidad):
+        if cantidad > 0:
+            self.__cantidad += cantidad
+    
+    def retirar(self, cantidad):
+        self.__cantidad -= cantidad
+
+print("Bienvenido a la cuenta BBVA!")   
+cuenta_kaz = Cuenta("Kaz", 55900.5)  
+person=Persona("Kaz", "Brekker")      
+print(person.nombre_completo)
+
+# Mostrar los datos de la cuenta
+cuenta_kaz.mostrar()
+
+# Ingresar 500 a la cuenta
+cuenta_kaz.ingresar(55900.5)
+cuenta_kaz.mostrar()
+
+# Retirar 1200 de la cuenta
+cuenta_kaz.retirar(1200.0)
+cuenta_kaz.mostrar()
---
+++ consigna8.py
@@ -0,0 +1,46 @@
+# 8. Vamos a definir ahora una “Cuenta Joven”, para ello vamos a crear una nueva clase
+# CuantaJoven que deriva de la clase creada en el punto 7. Cuando se crea esta nueva clase,
+# además del titular y la cantidad se debe guardar una bonificación que estará expresada en
+# tanto por ciento. Crear los siguientes métodos para la clase:
+# - Un constructor.
+# - Los setters y getters para el nuevo atributo.
+# - En esta ocasión los titulares de este tipo de cuenta tienen que ser mayor de edad, por lo
+# tanto hay que crear un método es_titular_valido() que devuelve verdadero si el titular es
+# mayor de edad pero menor de 25 años y falso en caso contrario.
+# - Además, la retirada de dinero sólo se podrá hacer si el titular es válido.
+# - El método mostrar() debe devolver el mensaje de “Cuenta Joven” y la bonificación de la cuenta.
+
+from consigna7 import Cuenta
+import datetime
+from datetime import date
+
+
+class CuentaJoven(Cuenta):
+    def __init__(self, titular, cantidad, bonificacion=0):
+        super().__init__(titular, cantidad)
+        self.__bonificacion = bonificacion
+    
+    @property
+    def bonificacion(self):
+        return self.__bonificacion
+    
+    @bonificacion.setter
+    def bonificacion(self, bonificacion):
+        self.__bonificacion = bonificacion
+    
+    def es_titular_valido(self):
+        # Suponemos que la fecha de nacimiento del titular se encuentra en el atributo fecha_nacimiento
+        edad = (datetime.date.today() - self.get_titular().fecha_nacimiento).days // 365
+        return edad > 18 and edad < 25
+    
+    def retirar(self, cantidad):
+        if self.es_titular_valido():
+            super().retirar(cantidad)
+    
+    def mostrar(self):
+        print("Cuenta Joven")
+        super().mostrar()
+        print("Bonificación: ", self.__bonificacion)
+
+cuenta_joven = CuentaJoven("Jack", 512000, 5)
+cuenta_joven.mostrar()
